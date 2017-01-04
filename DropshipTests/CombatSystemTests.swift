f99a2f6413a904ab83728f03b73618ae56b00b6c
Jason Baird <jason.baird@asynchrony.com>
2017-01-04 17:39:07 -0600

    Whoops. There. Actually renamed the thing. Last time I just deleted it.

--- DropshipTests/CombatSystemTests.swift
+++ DropshipTests/CombatSystemTests.swift
@@ -11,17 +11,17 @@ import XCTest
 
 class CombatSystemTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-    }
-    
     func testOneVsOneCombat() {
         var jasonWins = 0
         var codyWins = 0
         for _ in 1...100 {
-            let jason = Unit(name: "Jason", health: 5, damageResponders: [], activationResponders: [])
-            let cody = Unit(name: "Cody", health: 5, damageResponders: [], activationResponders: [])
-            let combat = Combat(activatables: [jason, cody])
+            let pistol = Weapon(damage: 3, ammo: 3, accuracy: 65)
+            let jason = Creature(name: "Jason", maxHealth: 5, abilities: [pistol])
+            let pistol2 = Weapon(damage: 3, ammo: 3, accuracy: 65)
+            let cody = Creature(name: "Cody", maxHealth: 5, abilities: [pistol2])
+            let combat = Initiative(squads: [Squad(readyables: [jason]), Squad(readyables: [cody])])
+            jason.targetStrategy = RandomTargetStrategy(damageables: [cody])
+            cody.targetStrategy = RandomTargetStrategy(damageables: [jason])
             combat.playCombat()
             
             if(!jason.dead) {
